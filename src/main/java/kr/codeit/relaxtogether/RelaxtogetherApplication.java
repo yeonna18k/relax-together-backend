15aa7e295e91c19e759d0ddcdeb2e5ca6472b383
JWbase <jungwoo1002@gmail.com>
2024-09-09 10:10:32 +0900

    first commit

---
+++ src/main/java/kr/codeit/relaxtogether/RelaxtogetherApplication.java
@@ -0,0 +1,13 @@
+package kr.codeit.relaxtogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelaxtogetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelaxtogetherApplication.class, args);
+	}
+
+}
