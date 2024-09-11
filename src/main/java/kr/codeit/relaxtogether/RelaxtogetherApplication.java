cc8241707c784477d09c6636ff183defae45c346
yonghyeonpark <pyh4616@naver.com>
2024-09-11 21:55:42 +0900

    feat: 생성 날짜, 수정 날짜 공통 설정

--- src/main/java/kr/codeit/relaxtogether/RelaxtogetherApplication.java
+++ src/main/java/kr/codeit/relaxtogether/RelaxtogetherApplication.java
@@ -2,7 +2,9 @@ package kr.codeit.relaxtogether;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class RelaxtogetherApplication {
 
