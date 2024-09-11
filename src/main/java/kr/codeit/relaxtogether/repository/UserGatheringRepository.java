d8d49c265c6789dddf8afab139b8fe91a3f810c3
yonghyeonpark <pyh4616@naver.com>
2024-09-11 22:23:19 +0900

    feat: User - Gathering 연관관계 매핑

---
+++ src/main/java/kr/codeit/relaxtogether/repository/UserGatheringRepository.java
@@ -0,0 +1,7 @@
+package kr.codeit.relaxtogether.repository;
+
+import kr.codeit.relaxtogether.entity.UserGathering;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserGatheringRepository extends JpaRepository<UserGathering, Long> {
+}
