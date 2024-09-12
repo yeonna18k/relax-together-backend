b875f09791f11427ed7878a3229bb6db1a44b59d
yonghyeonpark <pyh4616@naver.com>
2024-09-13 00:15:13 +0900

    feat: 스프링 시큐리티 설정 클래스 구현

---
+++ src/main/java/kr/codeit/relaxtogether/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package kr.codeit.relaxtogether.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.csrf(AbstractHttpConfigurer::disable)
+            .formLogin(AbstractHttpConfigurer::disable)
+            .httpBasic(AbstractHttpConfigurer::disable)
+            .logout(AbstractHttpConfigurer::disable)
+            .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests(
+                (auth) -> auth.requestMatchers("/auths/signup", "/auths/login").permitAll().anyRequest()
+                    .authenticated())
+            .build();
+    }
+}
