2fa63950aa80f94d874ad51189314e29fe90c8a9
yonghyeonpark <pyh4616@naver.com>
2024-09-16 00:00:48 +0900

    feat: User 이메일을 검증하는 API 구현

---
+++ src/main/java/kr/codeit/relaxtogether/controller/UserController.java
@@ -0,0 +1,24 @@
+package kr.codeit.relaxtogether.controller;
+
+import jakarta.validation.Valid;
+import kr.codeit.relaxtogether.dto.user.request.EmailCheckRequest;
+import kr.codeit.relaxtogether.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/auths")
+@RestController
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/check-email")
+    public ResponseEntity<Boolean> checkEmail(@Valid @RequestBody EmailCheckRequest emailCheckRequest) {
+        return ResponseEntity.ok(userService.checkEmail(emailCheckRequest));
+    }
+}
