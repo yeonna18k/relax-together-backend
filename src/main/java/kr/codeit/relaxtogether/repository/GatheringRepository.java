506af3e2e319026b278ddc11f19522e6fd5cdb65
JWbase <jungwoo1002@gmail.com>
2024-09-17 03:20:02 +0900

    refactor: repository package 이동

--- src/main/java/kr/codeit/relaxtogether/repository/GatheringRepository.java
+++ src/main/java/kr/codeit/relaxtogether/repository/GatheringRepository.java
@@ -1,6 +1,6 @@
 package kr.codeit.relaxtogether.repository;
 
-import kr.codeit.relaxtogether.entity.Gathering;
+import kr.codeit.relaxtogether.entity.gathering.Gathering;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GatheringRepository extends JpaRepository<Gathering, Long> {
