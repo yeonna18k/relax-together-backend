5bd9370a08ff8e6fec230154801299d80a99839d
JWbase <jungwoo1002@gmail.com>
2024-09-17 03:46:39 +0900

    refactor: Controller PostMapping 어노테이션 추가

--- src/main/java/kr/codeit/relaxtogether/controller/GatheringController.java
+++ src/main/java/kr/codeit/relaxtogether/controller/GatheringController.java
@@ -5,6 +5,7 @@ import kr.codeit.relaxtogether.dto.gathering.CreateGatheringRequest;
 import kr.codeit.relaxtogether.service.GatheringService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ public class GatheringController {
 
     private final GatheringService gatheringService;
 
+    @PostMapping
     public ResponseEntity<Void> createGathering(@Valid @RequestBody CreateGatheringRequest request) {
         gatheringService.createGathering(request);
         return ResponseEntity.ok().build();
