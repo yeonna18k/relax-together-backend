a1c9922bd1acb7eb5ca52f056eaaf814e8d07580
yonghyeonpark <pyh4616@naver.com>
2024-09-19 16:29:32 +0900

    refactor: 필터 등록 및 인증 관련 url 추가

--- src/main/java/kr/codeit/relaxtogether/config/SecurityConfig.java
+++ src/main/java/kr/codeit/relaxtogether/config/SecurityConfig.java
@@ -1,16 +1,34 @@
 package kr.codeit.relaxtogether.config;
 
+import kr.codeit.relaxtogether.auth.LoginFilter;
+import kr.codeit.relaxtogether.auth.jwt.JwtUtil;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@RequiredArgsConstructor
+@EnableWebSecurity
 @Configuration
 public class SecurityConfig {
+
+    private final AuthenticationConfiguration authenticationConfiguration;
+    private final JwtUtil jwtUtil;
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+        throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
@@ -26,9 +44,16 @@ public class SecurityConfig {
             .headers(headerConfig -> headerConfig.frameOptions(FrameOptionsConfig::sameOrigin))
             .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth ->
-                auth.requestMatchers("/auths/signup", "/auths/login", "/h2-console/**", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                auth.requestMatchers("/auths/check-email",
+                        "/auths/signup",
+                        "/auths/login",
+                        "/h2-console/**",
+                        "/swagger-ui/**",
+                        "/v3/api-docs/**").permitAll()
                     .anyRequest().authenticated()
             )
+            .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil),
+                UsernamePasswordAuthenticationFilter.class)
             .build();
     }
 }
