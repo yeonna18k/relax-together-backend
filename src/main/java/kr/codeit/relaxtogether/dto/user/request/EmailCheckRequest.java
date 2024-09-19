658854b66c5e725510d20192b581d67ba8abe560
yonghyeonpark <pyh4616@naver.com>
2024-09-19 16:24:41 +0900

    refactor: 역직렬화를 위한 기본 생성자 추가

--- src/main/java/kr/codeit/relaxtogether/dto/user/request/EmailCheckRequest.java
+++ src/main/java/kr/codeit/relaxtogether/dto/user/request/EmailCheckRequest.java
@@ -2,10 +2,13 @@ package kr.codeit.relaxtogether.dto.user.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class EmailCheckRequest {
 
     @NotBlank
