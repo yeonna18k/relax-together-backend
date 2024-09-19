72e505f7da6e32205050d1022db5badf13ce4928
yonghyeonpark <pyh4616@naver.com>
2024-09-19 16:22:39 +0900

    feat: UserDetails 인터페이스를 클래스로 구현

---
+++ src/main/java/kr/codeit/relaxtogether/auth/CustomUserDetails.java
@@ -0,0 +1,63 @@
+package kr.codeit.relaxtogether.auth;
+
+import java.util.Collection;
+import java.util.List;
+import kr.codeit.relaxtogether.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class CustomUserDetails implements UserDetails {
+
+    private final User user;
+
+    public CustomUserDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    public String getName() {
+        return user.getName();
+    }
+
+    public String getCompanyName() {
+        return user.getCompanyName();
+    }
+    
+    public String getProfileImage() {
+        return user.getProfileImage();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
