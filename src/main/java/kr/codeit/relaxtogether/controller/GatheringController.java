66241d3b83c3ca331947eb5db255ff94b2c647f1
JWbase <jungwoo1002@gmail.com>
2024-09-20 17:49:54 +0900

    refactor: api uri에 /api prefix 추가

--- src/main/java/kr/codeit/relaxtogether/controller/GatheringController.java
+++ src/main/java/kr/codeit/relaxtogether/controller/GatheringController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@RequestMapping("/gatherings")
+@RequestMapping("/api/gatherings")
 @RestController
 public class GatheringController {
 
