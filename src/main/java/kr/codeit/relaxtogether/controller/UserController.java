05a9ec9ed24311819a290f5ad5c393c86fb77d35
yonghyeonpark <pyh4616@naver.com>
2024-09-20 20:06:30 +0900

    refactor: URI 앞에 /api 추가

--- src/main/java/kr/codeit/relaxtogether/controller/UserController.java
+++ src/main/java/kr/codeit/relaxtogether/controller/UserController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@RequestMapping("/auths")
+@RequestMapping("/api/auths")
 @RestController
 public class UserController {
 
