2dba0920601d52a9903669f8149e2f6faafc5c0b
JWbase <jungwoo1002@gmail.com>
2024-09-20 17:18:33 +0900

    refactor: 모임 생성 비즈니스 로직 수정

--- src/main/java/kr/codeit/relaxtogether/service/GatheringService.java
+++ src/main/java/kr/codeit/relaxtogether/service/GatheringService.java
@@ -4,9 +4,11 @@ import kr.codeit.relaxtogether.dto.PagedResponse;
 import kr.codeit.relaxtogether.dto.gathering.request.CreateGatheringRequest;
 import kr.codeit.relaxtogether.dto.gathering.request.GatheringSearchCondition;
 import kr.codeit.relaxtogether.dto.gathering.response.SearchGatheringResponse;
+import kr.codeit.relaxtogether.entity.User;
 import kr.codeit.relaxtogether.entity.gathering.Gathering;
 import kr.codeit.relaxtogether.entity.gathering.UserGathering;
 import kr.codeit.relaxtogether.repository.UserGatheringRepository;
+import kr.codeit.relaxtogether.repository.UserRepository;
 import kr.codeit.relaxtogether.repository.gathering.GatheringRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
@@ -19,23 +21,19 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class GatheringService {
 
+    private final UserRepository userRepository;
     private final GatheringRepository gatheringRepository;
     private final UserGatheringRepository userGatheringRepository;
 
     @Transactional
-    public void createGathering(CreateGatheringRequest request) {
-
-        if (request.getRegistrationEnd().isAfter(request.getDateTime())) {
-            throw new IllegalArgumentException("모집 종료일은 모임 시작일 이전이어야 합니다.");
-        }
+    public void createGathering(CreateGatheringRequest request, String email) {
+        validateDateTime(request);
+        User user = getUserByEmail(email);
 
         Gathering gathering = request.toEntity();
         gatheringRepository.save(gathering);
 
-        UserGathering userGathering = UserGathering.builder()
-            .gathering(gathering)
-            .build();
-        userGatheringRepository.save(userGathering);
+        saveUserGathering(user, gathering);
     }
 
     public PagedResponse<SearchGatheringResponse> search(GatheringSearchCondition condition, Pageable pageable) {
@@ -46,5 +44,24 @@ public class GatheringService {
             gatherings.hasNext(),
             gatherings.getNumberOfElements()
         );
+    }
+
+    private void saveUserGathering(User user, Gathering gathering) {
+        UserGathering userGathering = UserGathering.builder()
+            .user(user)
+            .gathering(gathering)
+            .build();
+        userGatheringRepository.save(userGathering);
+    }
+
+    private User getUserByEmail(String userId) {
+        return userRepository.findByEmail(userId)
+            .orElseThrow(() -> new IllegalArgumentException("유저정보를 찾을 수 없습니다."));
+    }
+
+    private static void validateDateTime(CreateGatheringRequest request) {
+        if (request.getRegistrationEnd().isAfter(request.getDateTime())) {
+            throw new IllegalArgumentException("모집 종료일은 모임 시작일 이전이어야 합니다.");
+        }
     }
 }
