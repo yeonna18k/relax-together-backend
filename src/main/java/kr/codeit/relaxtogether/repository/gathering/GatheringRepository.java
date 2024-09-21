e5ff23824069cb492acfefeba94744cab9978307
JWbase <jungwoo1002@gmail.com>
2024-09-22 01:29:25 +0900

    feat: findByIdAndCreatedById 메서드 추가

--- src/main/java/kr/codeit/relaxtogether/repository/gathering/GatheringRepository.java
+++ src/main/java/kr/codeit/relaxtogether/repository/gathering/GatheringRepository.java
@@ -1,8 +1,10 @@
 package kr.codeit.relaxtogether.repository.gathering;
 
+import java.util.Optional;
 import kr.codeit.relaxtogether.entity.gathering.Gathering;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GatheringRepository extends JpaRepository<Gathering, Long>, GatheringRepositoryCustom {
 
+    Optional<Gathering> findByIdAndCreatedById(Long gatheringId, Long createdById);
 }
