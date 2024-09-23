42267ae0cec97b51bb3f33c99f99c4a4bd632bdc
JWbase <jungwoo1002@gmail.com>
2024-09-23 14:28:38 +0900

    feat: 개발 서버, 로컬 서버 분리

--- src/main/java/kr/codeit/relaxtogether/config/SwaggerConfig.java
+++ src/main/java/kr/codeit/relaxtogether/config/SwaggerConfig.java
@@ -1,5 +1,7 @@
 package kr.codeit.relaxtogether.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
@@ -7,6 +9,12 @@ import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+    servers = {
+        @Server(url = "https://dev.relax-together.shop", description = "개발 서버"),
+        @Server(url = "http://localhost:8080", description = "로컬 서버")
+    })
 
 @Configuration
 public class SwaggerConfig {
