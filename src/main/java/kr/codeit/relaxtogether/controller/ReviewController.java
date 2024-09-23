552430d5c5a46eb818caf02b400c288e6f10fdcb
yonghyeonpark <pyh4616@naver.com>
2024-09-23 15:53:36 +0900

    feat: 리뷰 작성 API 구현

---
+++ src/main/java/kr/codeit/relaxtogether/controller/ReviewController.java
@@ -0,0 +1,30 @@
+package kr.codeit.relaxtogether.controller;
+
+import kr.codeit.relaxtogether.auth.CustomUserDetails;
+import kr.codeit.relaxtogether.dto.review.request.WriteReviewRequest;
+import kr.codeit.relaxtogether.service.ReviewService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/reviews")
+@RestController
+public class ReviewController {
+
+    private final ReviewService reviewService;
+
+    @PostMapping
+    public ResponseEntity<String> writeReview(@RequestBody WriteReviewRequest writeReviewRequest,
+        @AuthenticationPrincipal CustomUserDetails userDetails) {
+        reviewService.writeReview(writeReviewRequest, userDetails.getUsername());
+        return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .body("success");
+    }
+}
