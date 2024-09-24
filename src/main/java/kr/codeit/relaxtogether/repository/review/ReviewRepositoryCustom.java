1e935ef41abba93d913ef23bfcbfe713d4148f51
yonghyeonpark <pyh4616@naver.com>
2024-09-24 19:06:35 +0900

    feat: 모임 id를 통해 해당 모임에 작성된 리뷰 리스트를 가져오는 쿼리 작성

--- src/main/java/kr/codeit/relaxtogether/repository/review/ReviewRepositoryCustom.java
+++ src/main/java/kr/codeit/relaxtogether/repository/review/ReviewRepositoryCustom.java
@@ -2,8 +2,11 @@ package kr.codeit.relaxtogether.repository.review;
 
 import java.util.List;
 import kr.codeit.relaxtogether.dto.review.response.ReviewDetailsResponse;
+import org.springframework.data.domain.Pageable;
 
 public interface ReviewRepositoryCustom {
 
     List<ReviewDetailsResponse> findReviewsByUserId(Long userId);
+
+    List<ReviewDetailsResponse> findReviewsByGatheringId(Long gatheringId, Pageable pageable);
 }
--- src/main/java/kr/codeit/relaxtogether/repository/review/ReviewRepositoryCustomImpl.java
+++ src/main/java/kr/codeit/relaxtogether/repository/review/ReviewRepositoryCustomImpl.java
@@ -9,6 +9,7 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
 import kr.codeit.relaxtogether.dto.review.response.ReviewDetailsResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 
 @RequiredArgsConstructor
 public class ReviewRepositoryCustomImpl implements ReviewRepositoryCustom {
@@ -33,6 +34,30 @@ public class ReviewRepositoryCustomImpl implements ReviewRepositoryCustom {
             .join(review.user)
             .join(review.gathering)
             .where(review.user.id.eq(userId))
+            .fetch();
+    }
+
+    @Override
+    public List<ReviewDetailsResponse> findReviewsByGatheringId(Long gatheringId, Pageable pageable) {
+        return queryFactory.select(
+                Projections.constructor(
+                    ReviewDetailsResponse.class,
+                    gathering.type,
+                    gathering.location,
+                    user.profileImage,
+                    user.name,
+                    review.score,
+                    review.comment,
+                    review.createdDate
+                )
+            )
+            .from(review)
+            .join(review.user)
+            .join(review.gathering)
+            .where(review.gathering.id.eq(gatheringId))
+            .orderBy(review.createdDate.desc())
+            .offset(pageable.getOffset())
+            .limit(pageable.getPageSize())
             .fetch();
     }
 }
