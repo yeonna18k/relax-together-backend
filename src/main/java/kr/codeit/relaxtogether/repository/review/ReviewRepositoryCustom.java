69ca61e12de5b285a0857305728bc50738145980
yonghyeonpark <pyh4616@naver.com>
2024-09-24 22:28:20 +0900

    refactor: 페이징 적용 및 무한 스크롤을 위한 hasNext 추가

--- src/main/java/kr/codeit/relaxtogether/repository/review/ReviewRepositoryCustom.java
+++ src/main/java/kr/codeit/relaxtogether/repository/review/ReviewRepositoryCustom.java
@@ -3,10 +3,11 @@ package kr.codeit.relaxtogether.repository.review;
 import java.util.List;
 import kr.codeit.relaxtogether.dto.review.response.ReviewDetailsResponse;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 
 public interface ReviewRepositoryCustom {
 
-    List<ReviewDetailsResponse> findReviewsByUserId(Long userId);
+    Slice<ReviewDetailsResponse> findReviewsByUserId(Long userId, Pageable pageable);
 
     List<ReviewDetailsResponse> findReviewsByGatheringId(Long gatheringId, Pageable pageable);
 }
