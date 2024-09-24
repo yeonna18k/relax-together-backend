f3163607c9dc8a0f1d473153e9dc20d57dfe4b0d
yonghyeonpark <pyh4616@naver.com>
2024-09-24 13:20:34 +0900

    feat: 리뷰 상세 내용 응답을 위한 Dto 구현

---
+++ src/main/java/kr/codeit/relaxtogether/dto/review/response/ReviewDetailsResponse.java
@@ -0,0 +1,32 @@
+package kr.codeit.relaxtogether.dto.review.response;
+
+import java.time.LocalDateTime;
+import kr.codeit.relaxtogether.entity.gathering.Location;
+import kr.codeit.relaxtogether.entity.gathering.Type;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ReviewDetailsResponse {
+
+    private String gatheringType;
+    private String gatheringLocation;
+    private String userProfileImage;
+    private String userName;
+    private int score;
+    private String comment;
+    private LocalDateTime createdDate;
+
+    @Builder
+    public ReviewDetailsResponse(Type gatheringType, Location gatheringLocation, String userProfileImage,
+        String userName, int score, String comment, LocalDateTime createdDate) {
+        this.gatheringType = gatheringType.getParentCategory() + " " + gatheringType.getText();
+        this.gatheringLocation = gatheringLocation.getText();
+        this.userProfileImage = userProfileImage;
+        this.userName = userName;
+        this.score = score;
+        this.comment = comment;
+        this.createdDate = createdDate;
+    }
+}
