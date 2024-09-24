91ab4b280c7bf9ff160768ed247b746465dce3ba
yonghyeonpark <pyh4616@naver.com>
2024-09-24 13:21:10 +0900

    feat: 유저 email을 통해 해당 유저가 작성한 리뷰 리스트를 조회하는 기능 구현

--- src/main/java/kr/codeit/relaxtogether/service/ReviewService.java
+++ src/main/java/kr/codeit/relaxtogether/service/ReviewService.java
@@ -1,6 +1,8 @@
 package kr.codeit.relaxtogether.service;
 
+import java.util.List;
 import kr.codeit.relaxtogether.dto.review.request.WriteReviewRequest;
+import kr.codeit.relaxtogether.dto.review.response.ReviewDetailsResponse;
 import kr.codeit.relaxtogether.entity.Review;
 import kr.codeit.relaxtogether.entity.User;
 import kr.codeit.relaxtogether.entity.gathering.Gathering;
@@ -28,5 +30,11 @@ public class ReviewService {
             .orElseThrow(RuntimeException::new);
         Review review = writeReviewRequest.toEntity(user, gathering);
         reviewRepository.save(review);
+    }
+
+    public List<ReviewDetailsResponse> getLoginUserReviews(String loginEmail) {
+        User user = userRepository.findByEmail(loginEmail)
+            .orElseThrow(RuntimeException::new);
+        return reviewRepository.findReviewsByUserId(user.getId());
     }
 }
