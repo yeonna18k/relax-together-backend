acb50acf2e577376c10f4f59dd6c2be0a03a2012
yonghyeonpark <pyh4616@naver.com>
2024-09-24 13:33:06 +0900

    test: 유저 email을 통해 해당 유저가 작성한 리뷰 리스트를 조회하는 기능 테스트

--- src/test/java/kr/codeit/relaxtogether/service/ReviewServiceTest.java
+++ src/test/java/kr/codeit/relaxtogether/service/ReviewServiceTest.java
@@ -3,9 +3,14 @@ package kr.codeit.relaxtogether.service;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.tuple;
 
+import java.util.List;
 import kr.codeit.relaxtogether.dto.review.request.WriteReviewRequest;
+import kr.codeit.relaxtogether.dto.review.response.ReviewDetailsResponse;
+import kr.codeit.relaxtogether.entity.Review;
 import kr.codeit.relaxtogether.entity.User;
 import kr.codeit.relaxtogether.entity.gathering.Gathering;
+import kr.codeit.relaxtogether.entity.gathering.Location;
+import kr.codeit.relaxtogether.entity.gathering.Type;
 import kr.codeit.relaxtogether.repository.UserRepository;
 import kr.codeit.relaxtogether.repository.gathering.GatheringRepository;
 import kr.codeit.relaxtogether.repository.review.ReviewRepository;
@@ -35,12 +40,8 @@ public class ReviewServiceTest {
     @Test
     void writeReview() {
         // given
-        User user = User.builder()
-            .email("test@test.com")
-            .build();
-        Gathering gathering = Gathering.builder()
-            .name("gathering")
-            .build();
+        User user = createUser("test@test.com", null);
+        Gathering gathering = createGathering("gathering", null, null);
 
         userRepository.save(user);
         Long gatheringId = gatheringRepository.save(gathering).getId();
@@ -60,5 +61,73 @@ public class ReviewServiceTest {
             .containsExactlyInAnyOrder(
                 tuple(user, gathering, 5, "comment")
             );
+    }
+
+    @DisplayName("유저 email을 이용해서 해당 유저가 작성한 리뷰들을 조회합니다.")
+    @Test
+    void getLoginUserReviews() {
+        // given
+        User userA = createUser("test@test.com", "userA");
+        User userB = createUser("test1@test.com", "userB");
+        User userC = createUser("test2@test.com", "userC");
+
+        Gathering gatheringA = createGathering("A", Type.MINDFULNESS, Location.HONGDAE);
+        Gathering gatheringB = createGathering("B", Type.OFFICE_STRETCHING, Location.KONDAE);
+
+        userRepository.save(userA).getId();
+        userRepository.save(userB);
+        userRepository.save(userC);
+        gatheringRepository.save(gatheringA);
+        gatheringRepository.save(gatheringB);
+
+        Review reviewA1 = createReview(userA, gatheringA, 5, "good");
+        Review reviewA2 = createReview(userA, gatheringB, 4, "so-so");
+        Review reviewB1 = createReview(userB, gatheringA, 5, "good");
+        Review reviewB2 = createReview(userB, gatheringB, 3, "not bad");
+        Review reviewC1 = createReview(userC, gatheringA, 1, "bad");
+
+        reviewRepository.save(reviewA1);
+        reviewRepository.save(reviewA2);
+        reviewRepository.save(reviewB1);
+        reviewRepository.save(reviewB2);
+        reviewRepository.save(reviewC1);
+
+        // when
+        List<ReviewDetailsResponse> reviews = reviewService.getLoginUserReviews("test@test.com");
+
+        // then
+        assertThat(reviews).hasSize(2)
+            .extracting(
+                "gatheringType", "gatheringLocation", "userProfileImage",
+                "userName", "score", "comment"
+            )
+            .containsExactlyInAnyOrder(
+                tuple("달램핏 마인드풀니스", "홍대입구", null, "userA", 5, "good"),
+                tuple("달램핏 오피스 스트레칭", "건대입구", null, "userA", 4, "so-so")
+            );
+    }
+
+    private User createUser(String email, String name) {
+        return User.builder()
+            .email(email)
+            .name(name)
+            .build();
+    }
+
+    private Gathering createGathering(String name, Type type, Location location) {
+        return Gathering.builder()
+            .name(name)
+            .type(type)
+            .location(location)
+            .build();
+    }
+
+    private Review createReview(User user, Gathering gathering, int score, String comment) {
+        return Review.builder()
+            .user(user)
+            .gathering(gathering)
+            .score(score)
+            .comment(comment)
+            .build();
     }
 }
