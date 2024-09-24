7ac14b9ae9f1990ee09ab931d8f3ee21edc59f05
yonghyeonpark <pyh4616@naver.com>
2024-09-24 19:53:57 +0900

    feat: 모임 id와 페이징 처리를 이용해서 해당 모임에 작성된 리뷰 리스트를 가져오는 API 구현

--- src/main/java/kr/codeit/relaxtogether/controller/ReviewController.java
+++ src/main/java/kr/codeit/relaxtogether/controller/ReviewController.java
@@ -1,18 +1,24 @@
 package kr.codeit.relaxtogether.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import java.util.List;
 import kr.codeit.relaxtogether.auth.CustomUserDetails;
 import kr.codeit.relaxtogether.dto.review.request.WriteReviewRequest;
 import kr.codeit.relaxtogether.dto.review.response.ReviewDetailsResponse;
 import kr.codeit.relaxtogether.service.ReviewService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -38,5 +44,21 @@ public class ReviewController {
         return ResponseEntity
             .status(HttpStatus.OK)
             .body(loginUserReviews);
+    }
+
+    @Operation(summary = "리뷰 목록 조회", description = "해당 모임에 작성된 리뷰 목록을 조회합니다.")
+    @GetMapping("/{gatheringId}")
+    public ResponseEntity<List<ReviewDetailsResponse>> getReviewsByGatheringId(
+        @PathVariable Long gatheringId,
+        @Parameter(description = "조회 시작 위치 (최소 0)")
+        @RequestParam(value = "page", defaultValue = "0") int page,
+        @Parameter(description = "한 번에 조회할 리뷰 수 (최소 1)")
+        @RequestParam(value = "size", defaultValue = "10") int size
+    ) {
+        Pageable pageable = PageRequest.of(page, size);
+        List<ReviewDetailsResponse> reviewsByGatheringId = reviewService.getReviewsByGatheringId(gatheringId, pageable);
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(reviewsByGatheringId);
     }
 }
