97cb739bd1113a52a53e6b0ac2fc44938c17cc73
yonghyeonpark <pyh4616@naver.com>
2024-09-26 22:55:05 +0900

    refactor: 변경 사항 반영 및 누락된 예외 처리 추가

--- src/main/java/kr/codeit/relaxtogether/service/ReviewService.java
+++ src/main/java/kr/codeit/relaxtogether/service/ReviewService.java
@@ -1,5 +1,9 @@
 package kr.codeit.relaxtogether.service;
 
+import static kr.codeit.relaxtogether.exception.ErrorCode.CANNOT_WRITE_REVIEW_AS_ORGANIZER;
+import static kr.codeit.relaxtogether.exception.ErrorCode.GATHERING_NOT_FOUND;
+import static kr.codeit.relaxtogether.exception.ErrorCode.USER_NOT_FOUND;
+
 import kr.codeit.relaxtogether.dto.PagedResponse;
 import kr.codeit.relaxtogether.dto.review.request.ReviewSearchCondition;
 import kr.codeit.relaxtogether.dto.review.request.WriteReviewRequest;
@@ -9,6 +13,7 @@ import kr.codeit.relaxtogether.dto.review.response.ReviewScoreCountResponse;
 import kr.codeit.relaxtogether.entity.Review;
 import kr.codeit.relaxtogether.entity.User;
 import kr.codeit.relaxtogether.entity.gathering.Gathering;
+import kr.codeit.relaxtogether.exception.ApiException;
 import kr.codeit.relaxtogether.repository.UserRepository;
 import kr.codeit.relaxtogether.repository.gathering.GatheringRepository;
 import kr.codeit.relaxtogether.repository.review.ReviewRepository;
@@ -30,16 +35,19 @@ public class ReviewService {
     @Transactional
     public void writeReview(WriteReviewRequest writeReviewRequest, String loginEmail) {
         User user = userRepository.findByEmail(loginEmail)
-            .orElseThrow(RuntimeException::new);
+            .orElseThrow(() -> new ApiException(USER_NOT_FOUND));
         Gathering gathering = gatheringRepository.findById(writeReviewRequest.getGatheringId())
-            .orElseThrow(RuntimeException::new);
+            .orElseThrow(() -> new ApiException(GATHERING_NOT_FOUND));
+        if (gathering.getHostUser().getEmail().equals(loginEmail)) {
+            throw new ApiException(CANNOT_WRITE_REVIEW_AS_ORGANIZER);
+        }
         Review review = writeReviewRequest.toEntity(user, gathering);
         reviewRepository.save(review);
     }
 
     public PagedResponse<ReviewDetailsResponse> getLoginUserReviews(String loginEmail, Pageable pageable) {
         User user = userRepository.findByEmail(loginEmail)
-            .orElseThrow(RuntimeException::new);
+            .orElseThrow(() -> new ApiException(USER_NOT_FOUND));
         Slice<ReviewDetailsResponse> reviewsByUserId = reviewRepository.findReviewsByUserId(user.getId(), pageable);
         return new PagedResponse<>(
             reviewsByUserId.getContent(),
