939007551af308f43e611527c268cb8007b84f06
yonghyeonpark <pyh4616@naver.com>
2024-10-12 03:35:23 +0900

    refactor: 메일 발송 기능에 템플릿과 이미지 반영

--- src/main/java/kr/codeit/relaxtogether/service/EmailService.java
+++ src/main/java/kr/codeit/relaxtogether/service/EmailService.java
@@ -6,14 +6,18 @@ import static kr.codeit.relaxtogether.exception.ErrorCode.USER_NOT_FOUND;
 import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
-import jakarta.mail.internet.MimeMessage.RecipientType;
 import kr.codeit.relaxtogether.auth.jwt.JwtUtil;
 import kr.codeit.relaxtogether.dto.email.response.TokenVerificationResponse;
+import kr.codeit.relaxtogether.entity.User;
 import kr.codeit.relaxtogether.exception.ApiException;
 import kr.codeit.relaxtogether.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring6.SpringTemplateEngine;
 
 @RequiredArgsConstructor
 @Service
@@ -21,28 +25,35 @@ public class EmailService {
 
     private final UserRepository userRepository;
     private final JwtUtil jwtUtil;
-    private final JavaMailSender javaMailSender;
+    private final SpringTemplateEngine templateEngine;
+    private final JavaMailSender mailSender;
 
     public void sendEmail(String email) {
-        // 해당 email이 존재하는지 확인
-        if (!userRepository.existsByEmail(email)) {
-            throw new ApiException(USER_NOT_FOUND);
-        }
+        User user = userRepository.findByEmail(email)
+            .orElseThrow(() -> new ApiException(USER_NOT_FOUND));
 
-        // 링크 검증을 위한 토큰 추가
-        MimeMessage message = javaMailSender.createMimeMessage();
         String token = jwtUtil.createEmailVerificationToken(email);
+
+        // 유저 이름과 요청 링크를 변수로 담음
+        // 해당 Context를 템플릿에 담고 문자열로 변환
+        Context context = new Context();
+        context.setVariable("userName", user.getName());
+        context.setVariable("resetLink", "https://relax-together.web.app/reset-password?token=" + token);
+        String text = templateEngine.process("email.html", context);
+
+        MimeMessage message = mailSender.createMimeMessage();
         try {
-            message.setRecipients(RecipientType.TO, email); // 수신자 이메일
-            message.setSubject("같이달램 비밀번호 변경"); // 이메일 제목
-            String content = "";
-            content += "<h3>" + "해당 링크를 클릭해서 비밀번호 변경을 진행해주세요." + "</h3>";
-            content +=
-                "<h1><a href=" + "'http://localhost:8080/api/verify-link?token=" + token + "'>비밀번호 변경 링크</a></h1>";
-            message.setText(content, "UTF-8", "html"); // 이메일 내용
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+
+            helper.setTo(email);
+            helper.setSubject("같이달램 비밀번호 재설정 안내");
+            helper.setText(text, true);
+            helper.addInline("logo-icon", new ClassPathResource("static/images/logo-icon.png"));
+            helper.addInline("logo-text", new ClassPathResource("static/images/logo-text.png"));
         } catch (MessagingException e) {
+
         }
-        javaMailSender.send(message); // 이메일 발송
+        mailSender.send(message); // 이메일 발송
     }
 
     public TokenVerificationResponse verifyToken(String token) {
