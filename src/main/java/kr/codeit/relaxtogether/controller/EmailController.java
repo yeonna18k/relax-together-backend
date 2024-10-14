b96cea16fcd2ecb7d52bee1336c8939a28137327
yonghyeonpark <pyh4616@naver.com>
2024-10-14 15:05:55 +0900

    feat: 회원가입을 위한 이메일 발송, 인증번호 검증 API 구현

--- src/main/java/kr/codeit/relaxtogether/controller/EmailController.java
+++ src/main/java/kr/codeit/relaxtogether/controller/EmailController.java
@@ -1,6 +1,7 @@
 package kr.codeit.relaxtogether.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import kr.codeit.relaxtogether.dto.email.request.CodeVerificationRequest;
 import kr.codeit.relaxtogether.dto.email.request.SendEmailRequest;
 import kr.codeit.relaxtogether.dto.email.request.TokenVerificationRequest;
 import kr.codeit.relaxtogether.dto.email.response.TokenVerificationResponse;
@@ -19,7 +20,7 @@ public class EmailController {
 
     private final EmailService emailService;
 
-    @Operation(summary = "이메일 발송", description = "비밀번호 변경 링크를 담은 이메일을 발송합니다.")
+    @Operation(summary = "이메일 발송(비밀번호 변경)", description = "비밀번호 변경 링크를 담은 이메일을 발송합니다.")
     @PostMapping("/email/password-change")
     public ResponseEntity<String> sendEmailForPasswordChange(@RequestBody SendEmailRequest sendEmailRequest) {
         emailService.sendEmailForPasswordChange(sendEmailRequest.getEmail());
@@ -33,5 +34,21 @@ public class EmailController {
         @RequestBody TokenVerificationRequest tokenVerificationRequest) {
         return ResponseEntity
             .ok(emailService.verifyToken(tokenVerificationRequest.getToken()));
+    }
+
+    @Operation(summary = "이메일 발송(회원가입 진행)", description = "회원가입 진행을 위한 인증 코드를 담은 이메일을 발송합니다.")
+    @PostMapping("/email/signup")
+    public ResponseEntity<String> sendEmailForSignup(@RequestBody SendEmailRequest sendEmailRequest) {
+        emailService.sendEmailForSignup(sendEmailRequest.getEmail());
+        return ResponseEntity
+            .ok("success");
+    }
+
+    @Operation(summary = "인증번호 검증", description = "회원가입 인증번호를 검증합니다.")
+    @PostMapping("/verify-code")
+    public ResponseEntity<String> verifyCode(@RequestBody CodeVerificationRequest codeVerificationRequest) {
+        emailService.verifyCode(codeVerificationRequest.getCode());
+        return ResponseEntity
+            .ok("success");
     }
 }
